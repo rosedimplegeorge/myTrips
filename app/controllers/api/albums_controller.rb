aec24a743bb5246e478c3cf1fd6a7471f2a89e2f
rosedimple <rosedimplegeorge@gmail.com>
2018-06-14 18:17:30 -0400

    Routes are Done on Model 2

--- app/controllers/api/albums_controller.rb
+++ app/controllers/api/albums_controller.rb
@@ -1,2 +1,34 @@
 class Api::AlbumsController < ApplicationController
+    
+    def index
+        @albums = Trip.find(params[:trip_id]).albums
+        render json: @albums
+    end
+
+    def show
+        @album = Album.find(params[:id])
+        render json: @album
+    end
+
+
+    def create
+        @trip = Trip.find(params[:trip_id])
+        @album = Album.create!(album_params)
+        render json: @album
+    end
+
+    def update
+        @trip = Trip.find(params[:trip_id])
+        @album = Album.find(params[:id])
+        @album.update!(album_params)
+        render json: @album
+    end
+
+    private 
+
+    def album_params
+        params.require(:album).permit(:title, :photo_url, :trip_id)
+    end
+
+
 end
